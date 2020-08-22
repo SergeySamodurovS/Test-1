860389b309c520c8be70373e2e4d73cc6bf3bfd4
anst <starininandrey@gmail.com>
2020-08-22 18:54:12 +0300

    Init

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int PositiveScore { set; get; }
+        private int NegativeScore { set; get; }
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        private void ChoiceWord()
+        {
+            using (var file = new FileStream("dict.json", FileMode.Open))
+            {
+                dictionary = JsonSerializer.DeserializeAsync<Dictionary<string, string>>(file).Result;
+            }
+
+            Label_Word.Text = dictionary.First().Key;
+            Button_Variant1.Content = "помогите";
+            Button_Variant2.Content = "опасность";
+            Button_Variant3.Content = "забыл";
+            Button_Variant4.Content = "ого";
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChoiceWord();
+        }
+
+        private void Button_Variant_OnClick(object sender, RoutedEventArgs e)
+        {
+            var temp = (Button)sender;
+            var word = dictionary.First().Value;
+            if (word == temp.Content.ToString())
+            {
+                Label_ScorePositive.Text = "Верно - 1";
+            }
+            else
+            {
+                Label_ScoreNegative.Text = "Неверно - 1";
+            }
+        }
+    }
+}
